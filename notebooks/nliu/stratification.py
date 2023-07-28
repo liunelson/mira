2393ec5c81f2106fe869b3089c82951ec429dd0f
liunelson <nliu@uncharted.software>
2023-07-28 04:27:03 +0000

    Stratify experiment for Catlab comparison

---
+++ notebooks/nliu/stratification.py
@@ -0,0 +1,96 @@
+# %%[markdown]
+# Author: Nelson Liu
+# 
+# Email: [nliu@uncharted.software](mailto:nliu@uncharted.software)
+
+# %%
+import sympy
+import itertools
+from mira.metamodel import *
+# from mira.metamodel import ControlledConversion, NaturalConversion, Concept, Template, TemplateModel
+from mira.modeling import Model
+# from mira.modeling.viz import GraphicalModel
+from mira.modeling.askenet.petrinet import AskeNetPetriNetModel
+
+# %%
+# Units
+person_units = lambda: Unit(expression=sympy.Symbol('person'))
+day_units = lambda: Unit(expression=sympy.Symbol('day'))
+per_day_units = lambda: Unit(expression=1/sympy.Symbol('day'))
+dimensionless_units = lambda: Unit(expression=sympy.Integer('1'))
+per_day_per_person_units = lambda: Unit(expression=1/(sympy.Symbol('day')*sympy.Symbol('person')))
+
+# Concepts
+concepts = {
+    'S': Concept(name = 'S', units = person_units(), identifiers = {'ido': '0000514'}),
+    'I': Concept(name = 'I', units = person_units(), identifiers = {'ido': '0000511'}),
+    'R': Concept(name = 'R', units = person_units(), identifiers = {'ido': '0000592'}),
+}
+
+# Parameters
+parameters = {
+    'beta': Parameter(name = 'beta', value = 0.4, units = per_day_units()),
+    'gamma': Parameter(name = 'gamma', value = 1.0/11.0, units = per_day_units()),
+}
+
+# Initial conditions
+initials = {
+    'S': Initial(concept = Concept(name = 'S'), value = 1_000 - 1),
+    'I': Initial(concept = Concept(name = 'I'), value = 1),
+    'R': Initial(concept = Concept(name = 'R'), value = 0),
+}
+
+# Symbols
+S, I, R, beta, gamma = sympy.symbols('S I R beta gamma')
+
+# Observables
+observables = {
+    'total_population': Observable(name = 'Total Population', expression = SympyExprStr(S) + SympyExprStr(I) + SympyExprStr(R))
+}
+
+# S -> I transition
+t1 = ControlledConversion(
+    subject = concepts['S'],
+    outcome = concepts['I'],
+    controller = concepts['R'],
+    rate_law = S * I * beta
+)
+
+# I -> R transition
+t2 = NaturalConversion(
+    subject = concepts['I'], 
+    outcome = concepts['R'],
+    rate_law = gamma * I
+)
+
+# %%
+# Build SIR model from templates
+sir_model = TemplateModel(
+    templates = [t1, t2],
+    parameters = parameters,
+    initials = initials,
+    time = Time(name = 't', units = day_units()),
+    observables = observables,
+    annotations = Annotations(name = 'SIR model from MIRA')
+)
+
+# Generate AMR JSON
+AskeNetPetriNetModel(Model(sir_model)).to_json_file('sir_model.json')
+
+# %%
+# Do stratification with 2 age groups
+
+sir_age_model = stratify(
+    sir_model,
+    key = 'age',
+    strata = ['A1', 'A2'],
+    structure = [],
+    cartesian_control = True,
+    params_to_stratify = {'beta', 'gamma'}
+)
+
+sir_age_model.annotations.name = 'SIR model from MIRA, 2-age stratified with MIRA'
+
+AskeNetPetriNetModel(Model(sir_age_model)).to_json_file('sir_age_model.json')
+
+# %%
