d8e4ec8686cc0cc75f217aa9d8d6fd9242ec30ee
liunelson <nliu@uncharted.software>
2024-02-05 21:20:07 +0000

    Remove template function

--- notebooks/nliu/interconvert_petrinet_amr_templates.py
+++ notebooks/nliu/interconvert_petrinet_amr_templates.py
@@ -252,3 +252,23 @@ p = os.path.join(PATH, "templates")
 templates_tm_, model_tm_ = convert_templates_into_model(p, save = True)
 
 # %%
+def remove_template(tm: TemplateModel, tnames: list = []) -> TemplateModel:
+
+    # Create new template model,
+    # skipping over templates with given names
+    tm_new = copy.deepcopy(tm)
+    tm_new.templates = []
+    for t in tm.templates:
+        if t.name in tnames:
+            continue
+        tm_new.templates.append(t)
+    
+    # Remove parameters only used in removed templates
+    tm_new.eliminate_unused_parameters()
+
+    # Ditto for initials
+    tm_new.initials = {i: c for i, c in tm_new.initials.items() if i in tm_new.get_concepts_name_map()}
+
+    return tm_new
+
+# %%
