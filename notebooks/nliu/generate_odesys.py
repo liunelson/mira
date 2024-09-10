f0e8b6cf2badbb88f1c4a0aa42ac34bf3c26ee3f
Nelson Liu <nliu@uncharted.software>
2024-09-10 16:05:16 -0400

    Added observables to generate_odesys

--- notebooks/nliu/generate_odesys.py
+++ notebooks/nliu/generate_odesys.py
@@ -29,7 +29,8 @@ from mira.sources.amr.petrinet import template_model_from_amr_json
 PATH = "./data/milestone_18_evaluation/scenario2"
 
 # %%
-def generate_odesys(model, latex: bool = False, latex_align: bool = False) -> list:
+# Generate system of equations from a MIRA template model
+def generate_odesys(model, observables: bool = True, latex: bool = False, latex_align: bool = False) -> list:
 
     odeterms = {var: 0 for var in model.get_concepts_name_map().keys()}
 
@@ -51,6 +52,15 @@ def generate_odesys(model, latex: bool = False, latex_align: bool = False) -> li
     finally:
         t = symb(time)
 
+    # Observables
+    if len(model.observables) != 0:
+        obs_eqs = [
+            sympy.Eq(sympy.Function(obs.name)(t), obs.expression.args[0])
+            if latex == False
+            else f"\\operatorname{{{obs.name}}}(t) = " + sympy.latex(obs.expression.args[0])
+            for obs in model.observables.values()
+        ]
+
     # Construct Sympy equations
     odesys = [
         sympy.Eq(sympy.diff(sympy.Function(var)(t), t), terms) 
@@ -58,42 +68,42 @@ def generate_odesys(model, latex: bool = False, latex_align: bool = False) -> li
         else sympy.latex(sympy.Eq(sympy.diff(sympy.Function(var)(t), t), terms))
         for var, terms in odeterms.items()
     ]
-    
+
+    if observables == True:
+        odesys += obs_eqs
+
     if (latex == True) & (latex_align == True):
         odesys = "\\begin{align*} \n    " + " \\\\ \n    ".join([eq.replace(" = ", " &= ") for eq in odesys]) + "\n\\end{align*}"
         # odesys = "\\begin{align*}     " + " \\\\    ".join([eq.replace(" = ", " &= ") for eq in odesys]) + "\\end{align*}"
 
     return odesys
 
-# %%
-# Load example models
-
-models = {}
-
-with open(os.path.join(PATH, "SIRHD_base.json"), "r") as f:
-    models["base"] = template_model_from_amr_json(json.load(f))
-
-with open(os.path.join(PATH, "SIRHD_base_testing_multivax.json"), "r") as f:
-    models["strat1"] = template_model_from_amr_json(json.load(f))
-
-with open(os.path.join(PATH, "SIRHD_base_testing_multivax_age.json"), "r") as f:
-    models["strat2"] = template_model_from_amr_json(json.load(f))
 
 # %%
-generate_odesys(models["base"], latex = False, latex_align = False)
+# Load example model
+with open("data/monthly_demo_202408/SIDARTHE model with observables.json", "r") as f:
+    model = template_model_from_amr_json(json.load(f))
 
 # %%
-generate_odesys(models["base"], latex = True, latex_align = False)
+# Generate Sympy expressions
+generate_odesys(model, observables = True, latex = False, latex_align = False)
 
 # %%
-print(generate_odesys(models["base"], latex = True, latex_align = True))
+# Generate LaTeX code (one per equation)
+generate_odesys(model, observables = True, latex = True, latex_align = False)
 
 # %%
-print(generate_odesys(models["strat1"], latex = True, latex_align = True))
+# Generate LaTeX code (one for entire set of equations)
+print(generate_odesys(model, observables = True, latex = True, latex_align = True))
 
 # %%
-odesys = generate_odesys(models["base"], latex = True, latex_align = False)
+odesys = generate_odesys(model, observables = True, latex = True, latex_align = False)
 
+# Remove `\\operatorname for plot
+odesys = [re.sub(r"(\\operatorname)", "", eq) for eq in odesys]
+odesys
+
+# %%
 fig, ax = plt.subplots(1, 1, figsize = (8, 8))
 for y, eq in zip(np.flip(np.linspace(0, 1, len(odesys))), odesys):
     __ = ax.text(0.0, y, "$" + eq + "$", fontsize = 12)
