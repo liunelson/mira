d5a87f04a3d3e46021dc00eeb22f1e6f2627f253
Nelson Liu <nliu@uncharted.software>
2024-12-09 15:19:06 -0500

    Not mass conserving model example

--- notebooks/nliu/test_equations.py
+++ notebooks/nliu/test_equations.py
@@ -1,5 +1,7 @@
 # %%
 import os
+import sympy
+import json
 
 from mira.metamodel import *
 from mira.modeling import Model
@@ -19,51 +21,63 @@ os.environ["MIRA_REST_URL"] = MIRA_REST_URL
 # infected = Concept(name="I", identifiers={"ido": "0000511"})
 # recovered = Concept(name="R", identifiers={"ido": "0000592"})
 
-susceptible = Concept(name="S")
-infected = Concept(name="I")
-recovered = Concept(name="R")
+concepts = {k: Concept(name = k, display_name = k) for k in 'SEIVM'}
+parameters = {p: Parameter(name = p, display_name = p, value = 1.0) for p in 'lkdabgmb'} | {f'{k}0': Parameter(name = f'{k}0', display_name = f'{k}0', value = 1.0) for k in concepts.keys()}
+initials = {k: Initial(concept = c, expression = sympy.Symbol(f'{k}0')) for k, c in concepts.items()}
 
+local_dict = {k: sympy.Symbol(k) for k in concepts.keys()} | {k: sympy.Symbol(k) for k in parameters.keys()}
 
 # %%
-model_A = TemplateModel(
+# Define a non-mass-conserving model
+
+model = TemplateModel(
     templates = [
         ControlledConversion(
             name = 'Infection',
-            subject = susceptible,
-            outcome = infected,
-            controller = infected
-        )
-    ],
-    parameters = {
-        'b': Parameter(name = 'b', value = 1.0)
-    }
-)
-
-model_A.templates[0].set_mass_action_rate_law('b')
-
-model_A.draw_jupyter()
-
-# %%
-model_B = TemplateModel(
-    templates = [
+            subject = concepts['S'],
+            outcome = concepts['E'],
+            controller = concepts['I'],
+            rate_law = safe_parse_expr('l * S * I', local_dict = local_dict)
+        ),
         NaturalConversion(
+            name = 'Incubation',
+            subject = concepts['E'],
+            outcome = concepts['I'],
+            rate_law = safe_parse_expr('k * E', local_dict = local_dict)
+        ),
+        NaturalDegradation(
             name = 'Recovery',
-            subject = infected,
-            outcome = recovered
+            subject = concepts['I'],
+            rate_law = safe_parse_expr('d * I', local_dict = local_dict)
+        ),
+        ControlledProduction(
+            name = 'Viral production',
+            outcome = concepts['V'],
+            controller = concepts['I'],
+            rate_law = safe_parse_expr('a * b * (1 - g) * I', local_dict = local_dict)
+        ),
+        NaturalProduction(
+            name = 'Natural birth',
+            outcome = concepts['S'],
+            rate_law = safe_parse_expr('b * S', local_dict = local_dict)
+        ),
+        GroupedControlledDegradation(
+            name = 'Mask supply',
+            subject = concepts['M'],
+            controllers = [concepts['S'], concepts['E'], concepts['I']],
+            rate_law = safe_parse_expr('m * (S + E + I)', local_dict = local_dict)
         )
     ],
-    parameters = {
-        'g': Parameter(name = 'g', value = 1.0)
-    }
+    parameters = parameters,
+    initials = initials,
+    observables = {'N': Observable(name = 'N', expression = safe_parse_expr('S + E + I', local_dict = local_dict))},
+    time = Time(name = 't', units = Unit(expression = safe_parse_expr('day')))
 )
 
-model_B.templates[0].set_mass_action_rate_law('g')
-
-model_B.draw_jupyter()
+model.draw_jupyter()
 
 # %%
-# Compose the two models together
+with open('./data/example_models/non_mass_conserved.json', 'w') as fp:
+    json.dump(template_model_to_petrinet_json(model), fp, indent = 4)
 
-model_AB = compose([model_A, model_B])
-
-model_AB.draw_jupyter()
+# %%
