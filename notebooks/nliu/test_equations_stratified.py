89e23ff460c05eca54c7000b6234ea2fc1497862
Nelson Liu <nliu@uncharted.software>
2025-02-20 15:31:17 -0500

    Eval debug

--- notebooks/nliu/test_equations_stratified.py
+++ notebooks/nliu/test_equations_stratified.py
@@ -23,47 +23,6 @@ os.environ["MIRA_REST_URL"] = MIRA_REST_URL
 # %%
 def generate_odesys(model, latex: bool = False, latex_align: bool = False) -> list:
 
-    odeterms = {var: 0 for var in sorted(model.get_concepts_name_map().keys())}
-
-    for t in model.templates:
-        if hasattr(t, "subject"):
-            var = t.subject.name
-            odeterms[var] -= t.rate_law.args[0]
-        
-        if hasattr(t, "outcome"):
-            var = t.outcome.name
-            odeterms[var] += t.rate_law.args[0]
-
-    # Time
-    symb = lambda x: sympy.Symbol(x)
-    try:
-        time = model.time.name
-    except:
-        time = "t"
-    finally:
-        t = symb(time)
-
-    # Construct equations
-    odesys = [
-        sympy.Eq(sympy.diff(sympy.Function(var)(t), t), terms) 
-        if latex == False
-        else sympy.latex(sympy.Eq(sympy.diff(sympy.Function(var)(t), t), terms))
-        for var, terms in odeterms.items()
-    ]
-    
-    if (latex == True) & (latex_align == True):
-        # odesys = "\\begin{align*} \n    " + " \\\\ \n    ".join([eq.replace(" = ", " &= ") for eq in odesys]) + "\n\\end{align*}"
-
-        odesys = "\\begin{align*} \n    " + " \\\\ \n    "
-        for eq in odesys:
-            [eq.replace(" = ", " &= ") for eq in odesys]
-            
-        odesys += "\n\\end{align*}"
-
-    return odesys
-
-def generate_odesys2(model, latex: bool = False, latex_align: bool = False) -> list:
-
     # State variables
     odeterms = {var: [] for var in sorted(model.get_concepts_name_map().keys())}
 
@@ -251,126 +210,10 @@ def post_mira_cleanup(mmt: TemplateModel) -> TemplateModel:
     return mmt
 
 # %%
-# Define concepts
-concepts = {k: Concept(name = k, display_name = k) for k in 'SEIVM'}
-parameters = {p: Parameter(name = p, display_name = p, value = 1.0) for p in 'lkdabgmb'} | {f'{k}0': Parameter(name = f'{k}0', display_name = f'{k}0', value = 1.0) for k in concepts.keys()}
-initials = {k: Initial(concept = c, expression = sympy.Symbol(f'{k}0')) for k, c in concepts.items()}
-local_dict = {k: sympy.Symbol(k) for k in concepts.keys()} | {k: sympy.Symbol(k) for k in parameters.keys()}
-
-# %%
-# Define a non-mass-conserving model
-model = TemplateModel(
-    templates = [
-        ControlledConversion(
-            name = 'Infection',
-            subject = concepts['S'],
-            outcome = concepts['E'],
-            controller = concepts['I'],
-            rate_law = safe_parse_expr('l * S * I', local_dict = local_dict)
-        ),
-        NaturalConversion(
-            name = 'Incubation',
-            subject = concepts['E'],
-            outcome = concepts['I'],
-            rate_law = safe_parse_expr('k * E', local_dict = local_dict)
-        ),
-        NaturalDegradation(
-            name = 'Recovery',
-            subject = concepts['I'],
-            rate_law = safe_parse_expr('d * I', local_dict = local_dict)
-        ),
-        ControlledProduction(
-            name = 'Viral production',
-            outcome = concepts['V'],
-            controller = concepts['I'],
-            rate_law = safe_parse_expr('a * b * (1 - g) * I', local_dict = local_dict)
-        ),
-        NaturalProduction(
-            name = 'Natural birth',
-            outcome = concepts['S'],
-            rate_law = safe_parse_expr('b * S', local_dict = local_dict)
-        ),
-        GroupedControlledDegradation(
-            name = 'Mask supply',
-            subject = concepts['M'],
-            controllers = [concepts['S'], concepts['E'], concepts['I']],
-            rate_law = safe_parse_expr('m * (S + E + I)', local_dict = local_dict)
-        )
-    ],
-    parameters = parameters,
-    initials = initials,
-    observables = {'N': Observable(name = 'N', expression = safe_parse_expr('S + E + I', local_dict = local_dict))},
-    time = Time(name = 't', units = Unit(expression = safe_parse_expr('day')))
-)
-
-model.draw_jupyter()
-
-# %%
-with open('./data/example_models/non_mass_conserved.json', 'w') as fp:
-    json.dump(template_model_to_petrinet_json(model), fp, indent = 4)
-
-# %%
-# Define model from equations
-# 
-# 0. Basic SIR
-# 1. With birth, death, controlled production
-# 2. Branching ratios 
-
-# %%
-# Model 0
-
-t = sympy.Symbol('t')
-S, E, I, R, V = sympy.symbols('S E I R V', cls = sympy.Function)
-l, k, d, a, b, g, m, b = sympy.symbols('l k d a b g m b')
-
-# %%
-odes = [
-    sympy.Eq(S(t).diff(t), - b * S(t) * I(t)),
-    sympy.Eq(I(t).diff(t), b * S(t) * I(t) - g * I(t)),
-    sympy.Eq(R(t).diff(t), g * I(t)),
-]
-
-model0 = template_model_from_sympy_odes(odes)
-
-print(generate_odesys(model0, latex = True, latex_align = True))
-
-with open('./data/model_equations/model0.json', 'w') as fp:
-    json.dump(template_model_to_petrinet_json(model0), fp, indent = 4)
-
-generate_summary_table(model0)
-
-# %%
-# Need note: 
-# - missing initials
-# - missing 't' unit
-# - missing template names
-# - missing parameter values
-
-# %%
-odes = [
-    sympy.Eq(S(t).diff(t), - b * S(t) * I(t) + l - m * S(t)),
-    sympy.Eq(I(t).diff(t), b * S(t) * I(t) - g * I(t) - m * I(t)),
-    sympy.Eq(R(t).diff(t), g * I(t) - m * R(t)),
-]
-
-model1 = template_model_from_sympy_odes(odes)
-model1.annotations = Annotations(name = 'SIR with births and deaths')
-
-print(generate_odesys(model1, latex = True, latex_align = True))
-
-with open('./data/model_equations/model1.json', 'w') as fp:
-    json.dump(template_model_to_petrinet_json(model1), fp, indent = 4)
-
-generate_summary_table(model1)
-
-# %%
-model1.draw_jupyter()
-
-# %%
-# Test with LaTeX-to-SymPy
+# Define base model
 
 odes_latex = [
-    r"\frac{d S(t)}{d t} = -b * S(t) * I(t) + l - m * S(t)",
+    r"\frac{d S(t)}{d t} = -b * S(t) * I(t) - m * S(t)",
     r"\frac{d I(t)}{d t} = b * S(t) * I(t) - g * I(t) - m * I(t)", 
     r"\frac{d R(t)}{d t} = g * I(t) - m * R(t)",
 ]
@@ -381,10 +224,11 @@ __ = [print(ode) for ode in odes_sympy]
 
 model = template_model_from_sympy_odes(odes_sympy)
 
-with open('./data/model_equations/model.json', 'w') as fp:
-    json.dump(template_model_to_petrinet_json(model), fp, indent = 4)
-
 generate_summary_table(model)
+
+# %%
+GraphicalModel.for_jupyter(model)
+
 
 # %%
 # Variation:
